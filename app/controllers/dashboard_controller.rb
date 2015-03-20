5a44fda89494f97b8453a35921f4c0f59a7e99d3
sibinx7 <sibin@redpanthers.co>
2015-03-20 10:21:53 +0530

    layouts, controllers

---
+++ app/controllers/dashboard_controller.rb
@@ -0,0 +1,29 @@
+class DashboardController < ApplicationController
+  render layout: "admin_dashboard"
+  def index
+  end
+
+  def users
+  end
+
+  def user
+  end
+
+  def projects
+  end
+
+  def project
+  end
+
+  def settings
+  end
+
+  def bids
+  end
+
+  def bid
+  end
+
+  def payments
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,36 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
+  get 'home/about'
+
+  get 'home/contact'
+
+  get 'home/maintenance'
+
+  get 'dashboard/index'
+
+  get 'dashboard/users'
+
+  get 'dashboard/user'
+
+  get 'dashboard/projects'
+
+  get 'dashboard/project'
+
+  get 'dashboard/settings'
+
+  get 'dashboard/bids'
+
+  get 'dashboard/bid'
+
+  get 'dashboard/payments'
+
   devise_for :admins
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root 'home#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
