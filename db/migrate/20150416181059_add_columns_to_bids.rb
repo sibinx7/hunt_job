2ee5979492daabf72aa385668ed635a9821614ee
Sibin Xavier <sibin@redpanthers.co>
2015-06-23 21:34:32 +0530

    Migration file deleted and change made on bid

--- db/migrate/20150416181059_add_columns_to_bids.rb
+++ db/migrate/20150416181059_add_columns_to_bids.rb
@@ -1,7 +1,7 @@
 class AddColumnsToBids < ActiveRecord::Migration
   def change
     add_column :bids , :status , :boolean
-    add_column :bids , :percentage , :boolean
+    add_column :bids , :percentage , :integer
     add_column :bids, :granted , :boolean
     add_column :bids , :accepted , :boolean
   end
--- db/migrate/20150417005845_change_column_type_in_bids.rb
+++
@@ -1,5 +0,0 @@
-class ChangeColumnTypeInBids < ActiveRecord::Migration
-  def change
-    change_column :bids , :percentage , :integer
-  end
-end
