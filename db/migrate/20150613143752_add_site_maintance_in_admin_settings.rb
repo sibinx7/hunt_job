dca6f390fb856debf0dae2abd1ef366cfe804cde
Sibin Xavier <sibin@redpanthers.co>
2015-06-23 21:56:33 +0530

    String to integer in site maintenance

--- db/migrate/20150613143752_add_site_maintance_in_admin_settings.rb
+++ db/migrate/20150613143752_add_site_maintance_in_admin_settings.rb
@@ -1,6 +1,6 @@
 class AddSiteMaintanceInAdminSettings < ActiveRecord::Migration
   def change
     add_column :admin_settings, :option_name ,:string
-    add_column :admin_settings, :option_value, :string
+    add_column :admin_settings, :option_value, :integer
   end
 end
